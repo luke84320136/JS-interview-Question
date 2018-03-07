2ba98a823ed40c3303f46292dea45daf60bd3af1
lukelan <luke84320136@gmail.com>
2018-03-07 16:09:48 +0800

    新增 3個 triple_add 範例

--- triple_add.js
+++ triple_add.js
@@ -12,3 +12,17 @@ function tripleAdd1(num1, num2, num3) {
   return num1 + num2 + num3
 }
 console.log(tripleAdd1(10, 20, 30))
+
+// EX1
+let tAdd = (num1, num2, num3) => num1 + num2 + num3
+console.log(tAdd(1, 2, 3))
+
+// EX2
+let tAdd2 = num1 => num2 => num3 => num1 + num2 + num3
+console.log(tAdd2(1)(2)(3))
+
+// EX3
+let tAdd3 = num1 => num2 => num3 => num1 + num2 + num3
+let number1 = tAdd3(1)
+let number2 = number1(2)
+console.log(number2(3))
